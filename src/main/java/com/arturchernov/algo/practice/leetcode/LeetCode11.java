3dad824f407f0b00cb8d67242ec733cffeddff8b
chernovav <artur.chernov@waveaccess.ru>
2021-02-18 23:21:10 +0500

    LeetCode 11: Container With Most Water

---
+++ src/main/java/com/arturchernov/algo/practice/leetcode/LeetCode11.java
@@ -0,0 +1,32 @@
+package com.arturchernov.algo.practice.leetcode;
+
+/**
+ * @author Artur Chernov
+ * <p>
+ * LeetCode 11. Container With Most Water
+ * <p>
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0).
+ * Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+ * <p>
+ * Notice that you may not slant the container.
+ */
+public class LeetCode11 {
+
+    public static void main(String[] args) {
+        System.out.println(maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+        System.out.println(maxArea(new int[]{1, 1}));
+        System.out.println(maxArea(new int[]{4, 3, 2, 1, 4}));
+        System.out.println(maxArea(new int[]{1, 2, 1}));
+    }
+
+    public static int maxArea(int[] height) {
+        int ans = 0;
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                ans = Math.max(Math.min(height[i], height[j]) * (j - i), ans);
+            }
+        }
+        return ans;
+    }
+}
