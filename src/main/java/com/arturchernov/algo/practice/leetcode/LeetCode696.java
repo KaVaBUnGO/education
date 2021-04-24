ab1e912d11fba93a78bb07f472d3deff93f5efad
chernovav <artur.chernov@waveaccess.ru>
2021-04-24 12:36:01 +0500

    LeetCode 696: Count Binary Substrings

---
+++ src/main/java/com/arturchernov/algo/practice/leetcode/LeetCode696.java
@@ -0,0 +1,59 @@
+package com.arturchernov.algo.practice.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Artur Chernov
+ * <p>
+ * LeetCode 696. Count Binary Substrings
+ * <p>
+ * Give a string s, count the number of non-empty (contiguous) substrings that have the same number of 0's and 1's,
+ * and all the 0's and all the 1's in these substrings are grouped consecutively.
+ * <p>
+ * Substrings that occur multiple times are counted the number of times they occur.
+ * <p>
+ * Example 1:
+ * Input: "00110011"
+ * Output: 6
+ * Explanation: There are 6 substrings that have equal number of consecutive 1's and 0's:
+ * "0011", "01", "1100", "10", "0011", and "01".
+ * <p>
+ * Notice that some of these substrings repeat and are counted the number of times they occur.
+ * <p>
+ * Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+ * Example 2:
+ * Input: "10101"
+ * Output: 4
+ * Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+ * Note:
+ * <p>
+ * s.length will be between 1 and 50,000.
+ * s will only consist of "0" or "1" characters.
+ */
+public class LeetCode696 {
+
+    public static void main(String[] args) {
+        System.out.println(countBinarySubstrings("00110011"));
+        System.out.println(countBinarySubstrings("10101"));
+    }
+
+    public static int countBinarySubstrings(String s) {
+        List<Integer> a = new ArrayList<>();
+        int cur = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                a.add(cur);
+                cur = 1;
+            }
+        }
+        a.add(cur);
+        int ans = 0;
+        for (int i = 1; i < a.size(); i++) {
+            ans += Math.min(a.get(i), a.get(i - 1));
+        }
+        return ans;
+    }
+}
